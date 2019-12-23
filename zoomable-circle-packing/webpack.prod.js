d5a4ac9bf53f721ee15d99bcb4ce9354d65f115f
moregorenine <moregorenine@gmail.com>
2019-12-24 00:43:20 +0900

    Feature Extract CSS & Minify HTML/CSS/JS

--- zoomable-circle-packing/webpack.prod.js
+++ zoomable-circle-packing/webpack.prod.js
@@ -2,6 +2,10 @@ const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
@@ -9,5 +13,47 @@ module.exports = merge(common, {
         filename: "[name].[contentHash].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
-    plugins: [new CleanWebpackPlugin()]
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./src/html/index.html",
+            chunks: ["index", "vendor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: "vendor.html",
+            template: "./src/html/vendor.html",
+            chunks: ["vendor"]
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin()
+            // new HtmlWebpackPlugin({
+            //     template: "./src/template.html",
+            //     minify: {
+            //         removeAttributeQuotes: true,
+            //         collapseWhitespace: true,
+            //         removeComments: true
+            //     }
+            // })
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, //3. Extract css into files
+                    // "style-loader", //3. Inject styles into DOM
+                    "css-loader", //2. Turns css into commonjs
+                    "sass-loader" //1. Turns sass into css
+                ]
+            }
+        ]
+    }
 });
